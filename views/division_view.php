bc671e24b69ff9c9b00b7e96f6ecc27aa449fa15
luzalcantar <luzalcantar1303@gmail.com>
2021-02-28 17:50:03 -0700

    feature/Operaciones Division

--- views/division_view.php
+++ views/division_view.php
@@ -5,11 +5,18 @@
     <div class="panel-body table-responsive">
         <?php 
             foreach ($resultados as $dato) { 
-                echo $dato['resultado'];
-                echo "<br>";
-                echo $dato['residuo'];
+                echo "<p>Operación: <strong>".$dato['dividendo']."/".$dato['divisor']."</strong></p>";
+                echo "<p>Resultado: <strong>".$dato['resultado']."</strong></p>";
+                echo "<p>Residuo: <strong>".$dato['residuo']."</strong></p>";
+
+                if($dato['residuo']>0){
+                    echo "<p class='mensaje'>El numero <strong>".$dato['dividendo']."</strong> no es divisible entre el número <strong>".$dato['divisor']."</strong></p>";
+                }else{
+                    echo "<p class='mensaje'>El numero <strong>".$dato['dividendo']."</strong> si es divisible entre el número <strong>".$dato['divisor']."</strong></p>";
+                }
             } 
         ?>
-        <p>Operación: </p>
+        
+        <a href="index.php" class="btn btn-primary btn-return">Volver al Inicio</a>
     </div>
 </div>
--- views/index.php
+++ views/index.php
@@ -6,9 +6,9 @@
         <form action='index.php?a=get_datos' method='post'>
             <div class="in_div">
                 <label for="in_dividendo">Dividendo:</label>   
-                <input type="number" class="form-control" name="in_dividendo" value="" min="1" required>
+                <input type="number" class="form-control" name="in_dividendo" value="" min="1" step="0.01" required>
                 <label for="in_divisor">Divisor:</label>
-                <input type="number" class="form-control" name="in_divisor" value="" min="1" required>
+                <input type="number" class="form-control" name="in_divisor" value="" min="1" step="0.01" required>
             </div>
             <input type='submit' class='btn btn-primary form-control btn-dividir' value='Dividir'>
         </form>
